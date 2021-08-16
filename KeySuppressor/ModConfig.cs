7d69d78919b2ed4fba01eea6e8ca16cab4e923e0
Deflaktor <deflaktor@gmail.com>
2021-08-16 14:41:34 +0200

    Initial Commit

---
+++ KeySuppressor/ModConfig.cs
@@ -0,0 +1,54 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeySuppressor
+{
+    class ModConfig
+    {
+        public enum SuppressMode
+        {
+            DoNotSuppress,
+            Suppress,
+            SuppressOnlyWhenInMenu,
+            SuppressOnlyWhenNotInMenu
+        }
+
+        public Dictionary<SButton, SuppressMode> SuppressedKeys { get; set; } = GetDefaultSuppressedKeys();
+
+        private static Dictionary<SButton, SuppressMode> GetDefaultSuppressedKeys()
+        {
+            var defaultSuppressedKeys = new Dictionary<SButton, SuppressMode>();
+            defaultSuppressedKeys.Add(SButton.DPadDown, true);
+            defaultSuppressedKeys.Add(SButton.DPadLeft, true);
+            defaultSuppressedKeys.Add(SButton.DPadRight, true);
+            defaultSuppressedKeys.Add(SButton.DPadUp, true);
+            defaultSuppressedKeys.Add(SButton.LeftShift, true);
+            defaultSuppressedKeys.Add(SButton.RightStick, true);
+            defaultSuppressedKeys.Add(SButton.ControllerA, false);
+            defaultSuppressedKeys.Add(SButton.ControllerB, false);
+            defaultSuppressedKeys.Add(SButton.ControllerX, false);
+            defaultSuppressedKeys.Add(SButton.ControllerY, false);
+            defaultSuppressedKeys.Add(SButton.ControllerBack, false);
+            defaultSuppressedKeys.Add(SButton.ControllerStart, false);
+            defaultSuppressedKeys.Add(SButton.BigButton, false);
+            defaultSuppressedKeys.Add(SButton.LeftShoulder, false);
+            defaultSuppressedKeys.Add(SButton.RightShoulder, false);
+            defaultSuppressedKeys.Add(SButton.LeftTrigger, false);
+            defaultSuppressedKeys.Add(SButton.RightTrigger, false);
+            defaultSuppressedKeys.Add(SButton.LeftThumbstickDown, false);
+            defaultSuppressedKeys.Add(SButton.LeftThumbstickLeft, false);
+            defaultSuppressedKeys.Add(SButton.LeftThumbstickRight, false);
+            defaultSuppressedKeys.Add(SButton.LeftThumbstickUp, false);
+            defaultSuppressedKeys.Add(SButton.RightThumbstickDown, false);
+            defaultSuppressedKeys.Add(SButton.RightThumbstickLeft, false);
+            defaultSuppressedKeys.Add(SButton.RightThumbstickRight, false);
+            defaultSuppressedKeys.Add(SButton.RightThumbstickUp, false);
+            return defaultSuppressedKeys;
+        }
+    }
+}
---
+++ KeySuppressor/ModEntry.cs
@@ -0,0 +1,49 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace KeySuppressor
+{
+    public class ModEntry : Mod
+    {
+
+        #region Properties
+        public static IMonitor MonitorObject { get; private set; }
+        private ModConfig Config;
+
+        #endregion
+
+
+        #region Entry
+        public override void Entry(IModHelper helper)
+        {
+            MonitorObject = Monitor;
+            Config = this.Helper.ReadConfig<ModConfig>();
+            helper.Events.Input.ButtonsChanged += SuppressKeys;
+
+        }
+
+        // Set very low (even lower than low) event priority so that this event is handled after all other mods have handled that event. That way the keys are only suppressed for the base game functionality.
+        #endregion
+        [EventPriority(EventPriority.Low - 10000)]
+        private void SuppressKeys(object sender, ButtonsChangedEventArgs e)
+        {
+            if (Config != null)
+            {
+                foreach (var keyValue in Config.SuppressedKeys)
+                {
+                    if (keyValue.Value)
+                    {
+                        this.Helper.Input.SuppressActiveKeybinds(KeybindList.ForSingle(keyValue.Key));
+                    }
+                }
+            }
+        }
+    }
+}
